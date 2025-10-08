df0814924475a973ca258c2563907269be1b65cf
Abdul Rafay <abdulrafaykhatana@gmail.com>
2025-10-08 15:05:14 +0500

    Initialize node project

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Welcome to my Git demo project!");
+
+function greetUser(name) {
+  return `Hello, ${name}!`;
+}
+
+const userName = "Abdul Rafay";
+console.log(greetUser(userName));
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(`2 + 3 = ${add(2, 3)}`);
